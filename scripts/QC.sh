a9d47ee2ae340dd6ad63cea859a2deb60675f0c2
noejean77-tech <noejean77@gmail.com>
2026-04-10 14:04:28 +0000

    Scritps

--- scripts/QC.sh
+++ scripts/QC.sh
@@ -2,15 +2,16 @@
 
 
 # definissons les variables
-BASE_DIR="/home/noe/cibig_noe/project_stage_tutore"
-RAW_DATA="$BASE_DIR/raw_data/merged_fastq"
-DIR_QC="$BASE_DIR/results/1_QC"
+
+RAW_DATA="/scratch/noe/sugarcane_projet/raw_data/merged_fastq"
+DIR_QC="/scratch/noe/sugarcane_projet/results/1_QC"
 
 mkdir -p "$DIR_QC"
 
 
 # telechargeons des modules
 echo "chargement des modules"
+module load bioinfo-wave 
 module load  nanoplot/1.41.3
 module load NanoComp/1.23.1
 
@@ -23,22 +24,9 @@ for file in "${RAW_DATA}"/*.fastq; do
 done
 
 
-# Comparaison QC globale
+# Comparaison QC globale (NanoComp)
 echo "Comparaison QC des 11 échantillons"
 
-# Construction des listes de fichiers et de noms
-FASTQ_FILES=()
-NAMES=()
-
-for file in "$RAW_DATA/"*.fastq; do
-    name=$(basename "$file" .fastq)
-    FASTQ_FILES+=("$file")
-    NAMES+=("$name")
-done
-
-NanoComp -t "$SLURM_CPUS_PER_TASK" \
-         --fastq "${FASTQ_FILES[@]}" \
-         --names "${NAMES[@]}" \
-         -o "${DIR_QC}/all_samples_QC"
+NanoComp -t "$SLURM_CPUS_PER_TASK" --fastq "$RAW_DATA"/*.fastq -o "$DIR_QC/all_samples_QC"
 
 echo "Analyse terminée !"
