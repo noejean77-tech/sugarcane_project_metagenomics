fc85a5def3c924ce5a86c7ad29faaa1c78a9d9a5
noejean77-tech <noejean77@gmail.com>
2026-04-15 12:40:13 +0000

    suppresion et ajouts des scripts

--- scripts/QC.sh
+++ scripts/QC.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-
-# definissons les variables
+# Let's define of variables
 
 RAW_DATA="/scratch/noe/sugarcane_projet/raw_data/merged_fastq"
 DIR_QC="/scratch/noe/sugarcane_projet/results/1_QC"
@@ -9,23 +8,23 @@ DIR_QC="/scratch/noe/sugarcane_projet/results/1_QC"
 mkdir -p "$DIR_QC"
 
 
-# telechargeons des modules
-echo "chargement des modules"
+# Dowload of modules
+echo "Download modules"
 module load bioinfo-wave 
-module load  nanoplot/1.41.3
+module load nanoplot/1.41.3
 module load NanoComp/1.23.1
 
 for file in "${RAW_DATA}"/*.fastq; do
   name=$(basename "$file" .fastq)
   echo "Debut d'analyse de $name"
-  # controle qualité des reads
+  # Quality control of reads
   echo "QC de $name"
   NanoPlot -t "$SLURM_CPUS_PER_TASK" --fastq "$file" -o "$DIR_QC/${name}"
 done
 
 
-# Comparaison QC globale (NanoComp)
-echo "Comparaison QC des 11 échantillons"
+# Overall QC Comparison (NanoComp)
+echo " QC of 11 échantillons"
 
 NanoComp -t "$SLURM_CPUS_PER_TASK" --fastq "$RAW_DATA"/*.fastq -o "$DIR_QC/all_samples_QC"
 
--- scripts/reorganized_data.sh
+++ scripts/reorganized_data.sh
@@ -1,28 +1,28 @@
 #!/bin/bash
 
-# Dossier source et destination
+# Source and destination folder
 
 SOURCE_DIR="/scratch/noe/sugarcane_projet/raw_data/Metagenomics/sugarecane_demux"
 Dest_dir="/scratch/noe/sugarcane_projet/raw_data"
 
-# Créer le dossier de destination pour chaque barcodes
+# Create the destination folder for each barcode.
 mkdir -p "$Dest_dir"
 
-# Boucle sur les barcodes (75 à 85)
+# Loop on the barcodes
 for b in {75..85}; do
-    # Créer le dossier spécifique pour le barcode (ex: barcode75)
+    # Create the specific folder for the barcode
     TARGET_DIR="$Dest_dir/barcode${b}"
     mkdir -p "$TARGET_DIR"
 
-    echo "Traitement du barcode ${b}..."
-# Trouvons tous les fichiers fastq des barcodes dans le dossier source puis copions ses fichiers dans 
-    # dans le dossier de destination dans les barcodes correspondantes
+    echo "Barcode processing ${b}..."
+# Let's find all the FastQ files for the barcodes in the source folder and then copy those files into 
+    # in the destination folder in the corresponding barcodes 
     find "$SOURCE_DIR" -type f -name "*barcode${b}*.fastq" | while read -r file; do
-        # On extrait le nom du run (ex: AQN321)
+        # We extract the name of the run (ex: AQN321)
       
         BASE_NAME=$(basename "$file")
 
-        # Copie et renommage du fichier 
+        # Copying and renaming the file 
         cp "$file" "$TARGET_DIR/${BASE_NAME}"
     done
 done
